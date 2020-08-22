0ac6db61f53e752825c01c6425f132c7d44232f3
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-22 12:52:18 -0500

    The navigation header was implemented.

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,11 +1,15 @@
 import React from 'react'
+
 import UserItem from "./UserItem.js";
 import './UsersList.css'
+import Card from '../../shared/components/UIElements/Card'
 
 const UsersList = props => {
     if (props.items.length === 0) {
         return <div className="center">
-            <h2>No user found.</h2>
+            <Card>
+                <h2>No user found.</h2>
+            </Card>
         </div>
     }
 
