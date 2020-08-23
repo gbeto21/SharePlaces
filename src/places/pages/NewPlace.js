f90bc25311bef9849d8e121015d1897d56afd75f
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-23 08:02:10 -0500

    Validatios to te input were implemented.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import Input from '../../shared/components/FormElements/Input'
-import './NewPlace.css'
+import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import './NewPlace.css';
 
 const NewPlace = () => {
-    return <form className="place-form">
-        <Input
-            type="text"
-            label="Title"
-            element="input"
-            validators={[]}
-            errorText="Please enter a valid title."
-        // onChange={}
-        />
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
     </form>
-}
+  );
+};
 
 export default NewPlace;
