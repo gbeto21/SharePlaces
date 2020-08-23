889c774a6070181982f6362213fc4df263047dff
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-22 21:23:59 -0500

    The form to add new place was created.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import Input from '../../shared/components/FormElements/Input'
+import './NewPlace.css'
+
 const NewPlace = () => {
-    return <h2>NewPlace Works!</h2>
+    return <form className="place-form">
+        <Input
+            type="text"
+            label="Title"
+            element="input"
+        // validators={[]}
+        // onChange={}
+        />
+    </form>
 }
 
 export default NewPlace;
