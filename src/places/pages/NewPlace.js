5c25f879930c80c972ab44862de059306cb54b67
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-29 15:17:29 -0500

    The user id no more longer is pass to the new place funtionality from the body of the request.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -48,7 +48,6 @@ const NewPlace = () => {
       formData.append('title', formState.inputs.title.value)
       formData.append('description', formState.inputs.description.value)
       formData.append('address', formState.inputs.address.value)
-      formData.append('creator', auth.userId)
       formData.append('image', formState.inputs.image.value)
       formData.append('lat', 40.7063069)
       formData.append('lng', -74.010239)
