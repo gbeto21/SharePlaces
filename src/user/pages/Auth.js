5de7fe5147987624a14c0e01030d7de7da18f746
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-29 10:54:30 -0500

    The funtionality of upload an image to a place, was implemeted.

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -129,7 +129,15 @@ const Auth = () => {
                         onInput={inputHandler}
                     />}
 
-                    {!isLoginMode && <ImageUpload center id="image" onInput={inputHandler} />}
+                    {
+                        !isLoginMode &&
+                        <ImageUpload
+                            center
+                            id="image"
+                            onInput={inputHandler}
+                            errorText="Please provide an image."
+                        />
+                    }
 
                     <Input
                         element="input"
