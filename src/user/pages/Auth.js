729477e78779f182fc716d8f401ce237e8a3d777
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-28 19:12:21 -0500

    The buttons UPDATE and DELETE just appear if the authenticaded user is the owner of the places. The min lenght of the password was increaded from 5 to 6 characters

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -138,8 +138,8 @@ const Auth = () => {
                         id="password"
                         type="password"
                         label="Password"
-                        validators={[VALIDATOR_MINLENGTH(5)]}
-                        errorText="Pleace enter a valid password, at least 5 characters."
+                        validators={[VALIDATOR_MINLENGTH(6)]}
+                        errorText="Pleace enter a valid password, at least 6 characters."
                         onInput={inputHandler}
                     />
                     <Button
